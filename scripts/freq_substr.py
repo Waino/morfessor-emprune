ed4890947d345107027a7a7eebd83c9873d6f9bd
Stig-Arne Gronroos <stig-arne.gronroos@aalto.fi>
2019-11-21 11:17:40 +0200

    improvements to scripts/freq_substr.py

--- scripts/freq_substr.py
+++ scripts/freq_substr.py
@@ -18,9 +18,19 @@ def substrs(seq, min_len=1, max_len=None, prefixes=None):
             yield seq[start:end]
 
 class FrequentSubstrings(object):
-    def __init__(self, lex_size, prefix_len):
+    def __init__(self,
+                 lex_size,
+                 prefix_len,
+                 prune_redundant,
+                 forcesplit_before,
+                 forcesplit_after):
         self.lex_size = lex_size
         self.prefix_len = prefix_len
+        self.prune_redundant_margin = prune_redundant
+        self.forcesplit_before = set(forcesplit_before)
+        self.forcesplit_after = set(forcesplit_after)
+
+        self.do_forcesplit = (len(self.forcesplit_before) + len(self.forcesplit_after)) > 0
         self.chars = collections.Counter()
         self.prefixes = collections.Counter()
         self.long = collections.Counter()
@@ -28,7 +38,8 @@ class FrequentSubstrings(object):
     def first_pass(self, seqs):
         # compute frequency of short substrings
         for count, seq in seqs:
-            self.chars.update(seq)
+            for char in seq:
+                self.chars[char] += count
             for substr in substrs(seq, max_len=self.prefix_len):
                 self.prefixes[substr] += count
         # prune out infrequent substrings
@@ -44,11 +55,59 @@ class FrequentSubstrings(object):
             for substr in substrs(seq, min_len=self.prefix_len + 1, prefixes=self.prefixes):
                 self.long[substr] += count
 
+    def _is_redundant(self, count, prefix_count):
+        return prefix_count <= (count + self.prune_redundant_margin)
+
+    def prune_redundant(self, seed_lexicon):
+        pruned = set()
+        for subw, count in seed_lexicon.items():
+            # test all prefixes, break early
+            for i in range(1, len(subw)):
+                prefix = subw[:-i]
+                prefix_count = seed_lexicon[prefix]
+                if self._is_redundant(count, prefix_count):
+                    pruned.add(prefix)
+                else:
+                    break
+            # test all suffixes, break early
+            for i in range(1, len(subw)):
+                suffix = subw[i:]
+                suffix_count = seed_lexicon[suffix]
+                if self._is_redundant(count, suffix_count):
+                    pruned.add(suffix)
+                else:
+                    break
+        for subw in pruned:
+            del seed_lexicon[subw]
+
+    def forcesplit(self, seed_lexicon):
+        pruned = set()
+        for subw in seed_lexicon:
+            if len(subw) == 1:
+                # always keep chars
+                continue
+            for char in subw[1:]:
+                # non-initial position
+                if char in self.forcesplit_before:
+                    pruned.add(subw)
+                    break
+            for char in subw[:-1]:
+                # non-final position
+                if char in self.forcesplit_after:
+                    pruned.add(subw)
+                    break
+        for subw in pruned:
+            del seed_lexicon[subw]
+
     def finalize(self):
         n_rare_chars = sum(1 for c in self.chars if c not in self.prefixes)
         prune_to = self.lex_size - n_rare_chars
         combined = self.prefixes
         combined.update(self.long)
+        if self.do_forcesplit:
+            self.forcesplit(combined)
+        if self.prune_redundant_margin >= 0:
+            self.prune_redundant(combined)
         # prune out infrequent substrings
         if len(combined) > prune_to:
             combined = collections.Counter(
@@ -66,8 +125,24 @@ class FrequentSubstrings(object):
 def get_parser():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--lex-size', type=int, default=25000)
-    parser.add_argument('--prefix', type=int, default=4)
+    parser.add_argument('--lex-size', type=int, default=1000000,
+        help='Size of seed lexicon')
+    parser.add_argument('--prefix', type=int, default=5,
+        help='Length of prefix to use in first pruning pass')
+    parser.add_argument('--prune-redundant', type=int, default=0,
+        help='Prune redundant prefix and suffix substrings. '
+             'Set to -1 to disable the pruning. '
+             'Set to 0 to prune only when counts are equal. '
+             'Set to a positive number to prune more aggressively. ')
+    parser.add_argument('--forcesplit-before', type=str, default='',
+        help='Characters to force splitting before. '
+             'Substrings with these chars in non-initial position will be pruned out.')
+    parser.add_argument('--forcesplit-after', type=str, default='',
+        help='Characters to force splitting after. '
+             'Substrings with these chars in non-final position will be pruned out.')
+    parser.add_argument('--forcesplit-both', type=str, default='',
+        help='Characters to force splitting both before and after. '
+             'Substrings longer than 1 with these chars will be pruned out.')
     return parser
 
 def main():
@@ -76,7 +151,14 @@ def main():
     lines = sys.stdin
     lines = [line.strip().split(None, 1) for line in lines]
     lines = [(int(count), word) for (count, word) in lines]
-    freq_substrs = FrequentSubstrings(args.lex_size, args.prefix)
+    forcesplit_before = args.forcesplit_before + args.forcesplit_both
+    forcesplit_after = args.forcesplit_after + args.forcesplit_both
+    freq_substrs = FrequentSubstrings(
+        args.lex_size,
+        args.prefix,
+        args.prune_redundant,
+        forcesplit_before,
+        forcesplit_after)
     freq_substrs.first_pass(lines)
     freq_substrs.second_pass(lines)
     combined = freq_substrs.finalize()
